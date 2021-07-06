59c649fb15bd32d9a76a8c0c6d657b14a4b60dcd
VineelaYemireddy <vinee.reddy.96@gmail.com>
2021-07-06 20:59:53 +1000

    setting for validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
 var express = require("express");
-var router = express.Router()
-const {signout, signup} = require("../controllers/auth");
+var router = express.Router();
+const { check, validationResult } = require('express-validator');
+const { signout, signup} = require("../controllers/auth");
 
-router.post("/signup", signup)
+router.post("/signup", [
+
+    check("name", "name should be at least 3 char").isLength({min: 3 }),
+     check("name", "email is required").isEmail(),
+     check("password", "password should be at least 3 char").isLength({min: 3})
+    ], signup);
 router.get("/signout", signout);
     
 
