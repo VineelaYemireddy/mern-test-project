ff0cdd62a19a608cd6fd6d0cd2ed3901e68b72f3
VineelaYemireddy <vinee.reddy.96@gmail.com>
2021-07-11 01:29:29 +1000

    writing custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -74,3 +74,23 @@ exports.isSignedIn = expressJwt({
   secret: process.env.SECRET,
   userProperty: "auth"
 });
+
+//custom middleware
+exports.isAuthenticated = (req, res, next) => {
+   let checker = req.profile && req.auth && req.profile_id === req.auth._id;
+   if(!checker){
+     return res.status(403).json({
+       error: "ACCESS DENIED"
+     })
+   }
+  next();
+}
+
+exports.isAdmin = (req, res, next) => {
+  if(req.profile.role === 0){
+    return res.status(403).json({
+      error: "You are not ADMIN, Access denied"
+    });
+  }
+  next();
+};
