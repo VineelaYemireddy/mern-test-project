172e5b9625a365d53af5741ca6513da98ab173cf
VineelaYemireddy <vinee.reddy.96@gmail.com>
2021-08-04 12:15:12 +1000

    creating temporary route for using photo

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,29 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
-const {getProductById} = require("../controllers/product");
-const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
-const {getUserById} = require("../controllers/user");
+const {
+  getProductById,
+  createProduct,
+  getProduct,
+  photo
+} = require("../controllers/product");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
 
 //all of params
-router.param("userId, getUserById");
+router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 //all of actual routes
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
+
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
 module.exports = router;
